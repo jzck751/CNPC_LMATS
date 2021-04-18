c28cda2fd252059c10620d9401f3714d318d48b7
@一杯空氣 <61610679+jzck751@users.noreply.github.com>
2021-04-18 19:43:58 +0800

    4_18

---
+++ Model/Base_Class/Form_B_threePeak_and_fivePeak.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Base_Class
+{
+    //三峰法和五峰法的父类
+    public class Form_B_threePeak_and_fivePeak
+    {
+        public int serial_number { get; set; }//序号
+        public double well_depth { get; set; }//井深
+        public string the_lithology_named { get; set; }//岩心定名
+        public string samplr_type { get; set; }//样品类型
+        public double S0 { get; set; }//S0
+    }
+}
---
+++ Model/Form_B/Form_B_2_1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Form_B
+{
+    //井段
+    public class Form_B_2_1
+    {
+        public double top_depth { get; set; }//顶深
+        public double botton_depth { get; set; }//底深
+    }
+}
