f46474e2a25b8e2c73771ed868843b703a6e727d
Carter-zh <58615710+Carter-zh@users.noreply.github.com>
2021-05-17 00:03:01 +0800

    主界面UI完善

--- UIWPF/Resources/Windows/DataCollection_EditPanel.xaml.cs
+++ UIWPF/Resources/Windows/DataCollection_EditPanel.xaml.cs
@@ -39,6 +39,7 @@ namespace UIWPF.Resources.Windows
             connectionString.Database = "cnpc_lmats";
             connectionString.AllowUserVariables = true;
             MySqlConnection connection = new MySqlConnection(connectionString.ToString());
+            //TODO
             MySqlCommand cmd = new MySqlCommand("select * from mc_rock_describe_record", connection);
             MySqlDataAdapter adapter = new MySqlDataAdapter(select, connection);
             MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
@@ -61,6 +62,122 @@ namespace UIWPF.Resources.Windows
             
             
 
+        }
+
+        #region 标题栏事件
+
+        /// <summary>
+        /// 窗口移动事件
+        /// </summary>
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        int i = 0;
+        /// <summary>
+        /// 标题栏双击事件
+        /// </summary>
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            i += 1;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += (s, e1) => { timer.IsEnabled = false; i = 0; };
+            timer.IsEnabled = true;
+
+            if (i % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                i = 0;
+                this.WindowState = this.WindowState == WindowState.Maximized ?
+                              WindowState.Normal : WindowState.Maximized;
+
+            }
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //设置窗口最小化
+        }
+
+        /// <summary>
+        /// 窗口最大化与还原
+        /// </summary>
+        private void btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal; //设置窗口还原
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized; //设置窗口最大化
+            }
+        }
+
+        /// <summary>
+        /// 窗口关闭
+        /// </summary>
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (MessageBox.Show("确认要退出吗？", "登出确认", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                if (ifSave())
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    Window signOutCheck = new Resources.Windows.SignOutSaveCheck();
+                    signOutCheck.ShowDialog();
+                }
+
+            }
+        }
+
+        private bool ifSave()
+        {
+            return false;
+        }
+
+
+        #endregion 标题栏事件
+
+        private void sizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                MaxBtn.Content = "\xE923";
+            }
+            else
+            {
+                MaxBtn.Content = "\xE922";
+            }
+        }
+
+        private void sign_out_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("确认要登出账户吗？", "登出确认", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                if (ifSave())
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    Window signOutCheck = new Resources.Windows.SignOutSaveCheck();
+                    signOutCheck.ShowDialog();
+                }
+
+            }
         }
 
     }
